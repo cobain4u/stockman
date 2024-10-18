acf06ce4c025647537dc63c07a75bf7923d9f282
cobain4u <57416104+cobain4u@users.noreply.github.com>
2024-10-18 12:29:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+
 from inv import Item
 
  	 
 item=Item("coco nut",3,3,'baking')#this creats an item
-item.newItem()
-print(item.getName())
-
+dic={}
+while True:
+	item.newItem()
+	dic={item.getQuantity():item.getName()}
+	break
+	
+	print(dic)
+	
 	
 	
 	
