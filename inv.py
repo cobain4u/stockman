c752546080156622404afc09f0be67f9416a8a20
slayerbug <57416104+cobain4u@users.noreply.github.com>
2026-06-07 14:34:09 -0700

    Update inv.py

--- inv.py
+++ inv.py
@@ -1,9 +1,10 @@
-
+  ##crates objects######
 	class Item:
 	def __init__(self,name,price):
 		self.name=name
 		self.price=price
 	
 	
-####### input new item and set a departement for the item######
+	
+
 	
