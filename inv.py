f28d2b57b315f02a6c556b8dcb53ad44b813b869
slayerbug <57416104+cobain4u@users.noreply.github.com>
2026-06-07 14:31:21 -0700

    Update inv.py

--- inv.py
+++ inv.py
@@ -1,44 +1,9 @@
 
 	class Item:
-	def __init__(self,name,price,quantity,d):
+	def __init__(self,name,price):
 		self.name=name
-		self.price=float(price)
-		self.quantity=int(quantity)
-		self.d=d
+		self.price=price
 	
-	def getName(self):
-		return self.name
-	def getQuantity(self):
-		return self.quantity
+	
 ####### input new item and set a departement for the item######
-	def newItem(self):
-		while True:
-		#g=department()
-			name=input('please enter an item''\n')
-			self.name=name
-			quantity=input("how many do you need?"'\n')
-			self.quantity=quantity
-			department=self.d
-			
-		
-			options=input('would you like to add another item? 1= yes 2 = no''\n')
-			if(options=='1'):
-				new_name=name
-				amount=quantity
-				
-			elif (options=='2'):
-				print(name+":" 
-+ " " + quantity)
-				break
-			else:
-				print('invalid option please hit 1 or 2')
-					
-				
-	def makelist(self):
-	 return this.getName()
-	
-	
-		
-      
-	
 	
